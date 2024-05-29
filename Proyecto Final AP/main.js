34abdd7dbd4166a88d8a6146fd88e24f54c0df91
Tahiel Martín Devoto <tahielmartind16@gmail.com>
2024-05-29 15:07:23 -0300

    Creacion del array de objetos productos, mas creacion de las cards con js.

--- Proyecto Final AP/main.js
+++ Proyecto Final AP/main.js
@@ -1,3 +1,129 @@
 let productos =[{
-    nombre:'Cubo Rubik '
+    nombre:'Cubo Rubik 3x3 Valk Elite M',
+    descripcion:'El Valk 3 Elite M es el aclamado cubo insignia de la famosa línea Valk de QiYi MoFangGe. Ligero y rápido con un tacto suave y cómodo, el Valk 3 Elite M es el preferido por muchos cubers que disfrutan de un giro rápido con una sensación magnética uniforme y consistente. El Valk Elite M viene con tuercas transparentes (fuerza estándar) e imanes transparentes en las tapas centrales (fuerza estándar). Este cubo cuenta con un nuevo sistema magnético con tres niveles diferentes de resistencia. Las versiones moderada y fuerte cuentan con un esquema "magnético doble" y utilizan magnetismo de "borde central" más "borde de esquina" para el posicionamiento. Además, QiYi introdujo su propia versión de un sistema de ajuste de elasticidad que también reduce convenientemente el ruido del resorte.',
+    stock:20,
+    disponible:true,
+    cantidadSolicitada:0,
+    categoria:'3x3',
+    precioUnitario:47000,
+    calificacion:4.9,
+    imagen:'./estilos/img/3x3/ValkEliteM.webp'
+},{
+   nombre:'Cubo Rubik 3x3 Gan 356M',
+   descripcion:'El GAN 356 M es un moderno cubo magnético de 3x3 con una velocidad de rango medio, del popular fabricante GAN. Esta versión viene con el exclusivo núcleo IPG numérico ajustable a mano de GAN y un juego de tuercas GES de repuesto. Fuera de la caja, el GAN 356 M tiene una sensación rápida y estable que recuerda mucho a una mezcla entre el GAN XS y el GAN X V2, dos de los productos insignia más famosos de GAN. El GAN 356 M cumple con todas las necesidades que tienen los speedcubers para un bonito 3x3 magnético, y es una buena opción para los cubers a los que les gusta la ingeniería GAN con un toque de simplicidad.',
+   categoria:'3x3',
+   stock:20,
+   cantidadSolicitada:0,
+   precioUnitario:27000,
+   calificacion:5,
+   imagen:'./estilos/img/3x3/Gan356M.webp',
+   disponible:true
+   
+},{
+    nombre:'Cubo Rubik 2x2 Gan 249V2',
+   descripcion:'La GAN 249 V2 es la versión nueva y mejorada de la GAN 249. Este cubo de velocidad de 2x2 presenta un esquema de color stickerless más brillante, componentes internos de plástico primario, un diseño de panal en los puntos de contacto de las esquinas y una sensación general más suave. Se empaqueta dentro de una caja especial de GAN con una herramienta tensora de plástico y un folleto GAN CFOP.',
+   categoria:'2x2',
+   stock:40,
+   cantidadSolicitada:0,
+   precioUnitario:15000,
+   calificacion:4.6,
+   imagen:'./estilos/img/2x2/Gan249V2.webp',
+   disponible:true
+}, {
+    nombre:'Cubo Rubik 2x2 Moyu Meilong',
+    descripcion:'es un nuevo cubo de velocidad económico de 2x2 de MoFang JiaoShi. Este rompecabezas presenta tonos brillantes vibrantes sin pegatinas, giro rápido nada más sacarlo de la caja y una sensación de ligereza y amplitud al girar.',
+    categoria:'2x2',
+    stock:32,
+    cantidadSolicitada:0,
+    precioUnitario:8000,
+    calificacion:4.3,
+    imagen:'./estilos/img/2x2/moyuMeilong.webp',
+    disponible:true 
+},{
+    nombre:'Cubo Rubik 4x4 Qiyi MS',
+   descripcion:'El QiYi MS 4x4 es un cubo magnético de velocidad 4x4 construido para el rendimiento y la economía. Como parte de la nueva línea económica "MS" de QiYi, este 4x4 presenta una sensación rápida y ligeramente crujiente que hace que sea un placer girar. El MS 4x4 es una de nuestras principales recomendaciones para un gran 4x4 económico y sensato.',
+   categoria:'4x4',
+   stock:45,
+   cantidadSolicitada:0,
+   precioUnitario:11000,
+   calificacion:4.5,
+   imagen:'./estilos/img/4x4/QiyiMS.webp',
+   disponible:true
+},{
+    nombre:'Cubo Rubik 4x4 YJ MGC',
+   descripcion:'Es un nuevo cubo magnético de velocidad 4x4 de YJ. Este rompecabezas ofrece un rendimiento emblemático a una fracción del precio de un cubo alta gama. Cuenta con un giro suave y una sensación magnética media.',
+   categoria:'4x4',
+   stock:38,
+   cantidadSolicitada:0,
+   precioUnitario:20000,
+   calificacion:5,
+   imagen:'./estilos/img/4x4/yjMgc.webp',
+   disponible:true
+},{
+    nombre:'Qiyi Magnetic Clock',
+    descripcion:'Del famoso fabricante de speedcube QiYi MoFangGe, este nuevo y exquisito Clock es totalmente magnético, con 204 pequeños imanes incrustados en los engranajes y 4 grandes imanes dentro de los pines. El exterior de este nuevo reloj está diseñado con intrincadas incrustaciones en lugar de la impresión tradicional. Para la comodidad del usuario, QiYi redondeó las ruedas del reloj para que se pueda usar fácilmente sin raspar los dedos. Fascinantemente, el clock QiYi se puede tensar ajustando sus cuatro tornillos. Él clock QiYi también se puede abrir fácilmente para lubricación y mantenimiento.',
+    categoria:'Clock',
+    stock:90,
+    cantidadSolicitada:0,
+    precioUnitario:33000,
+    calificacion:5,
+    imagen:'./estilos/img/clock/qiyiMagneticClock.webp',
+    disponible:true
+},{
+    nombre:'Sheng Shou Magic M Clock',
+    descripcion:'El clock magnético ShengShou es un nuevo clock fabricado por ShengShou con un sistema único de estabilización magnética. El clock magnético ShengShou ofrece retroalimentación táctil y de clic al girar los engranajes. También conocido como "clock mágico", el clock magnético ShengShou cuenta con pasadores imantados encerrados en la carcasa del clock tradicional, lo que brinda una experiencia completamente nueva mientras resuelve el clock mágico.',
+    categoria:'Clock',
+    stock:98,
+    cantidadSolicitada:0,
+    precioUnitario:10000,
+    calificacion:4.1,
+    imagen:'./estilos/img/clock/shengShouMagicM.webp',
+    disponible:true
+},{
+    nombre:'Pyraminx moyu RS Maglev',
+    descripcion:'El MoYu RS Pyraminx (MagLev) ofrece un giro rápido y un buen rendimiento desde el primer momento. Este rompecabezas cuenta con el sistema de ajuste dual que te permite personalizar tanto la tensión como la compresión.',
+    categoria:'Pyraminx',
+    stock:36,
+    cantidadSolicitada:0,
+    precioUnitario:14000,
+    calificacion:4.1,
+    imagen:'./estilos/img/pyraminx/moyuRSMaglev.webp',
+    disponible:true
+},{
+    nombre:'Pyraminx qiyi Xman MB V2',
+    descripcion:'El Xman Bell V2 es el sucesor del Xman Bell original, que fue ampliamente considerado el mejor pyraminx esencialmente desde su lanzamiento, ha habido pocos retadores, pero finalmente hay una actualización que promete dejarlo atrás.',
+    categoria:'Pyraminx',
+    stock:29,
+    cantidadSolicitada:0,
+    precioUnitario:18000,
+    calificacion:4.9,
+    imagen:'./estilos/img/pyraminx/qiyiXmanMBV2.webp',
+    disponible:true
+
+
+
+},{
+    nombre:'Cubo Rubik Yuxin Little Magic Square-1 Sq-1 Magnético',
+    descripcion:'El Yuxin Little Square-1 Magnético es el primer Square-1 magnetico del mercado, tiene 32 imanes de neodimio que hacen de este SQ-1 uno de los mejores del mercado en cuanto a rendimiento se refiere, es rápido pero controlable cosa muy difícil de conseguir en un SQ-1.',
+    categoria:'sq-1',
+    stock:40,
+    cantidadSolicitada:0,
+    precioUnitario:22000,
+    calificacion:4.0,
+    imagen:'./estilos/img/sq1/yuxinLittleMagic.webp',
+    disponible:true
+},{
+    nombre:"Cubo Rubik YJ MGC Square-1 M",
+    descripcion:'El YJ MGC Square-1M es un emocionante rompecabezas de YJ, el fabricante de la línea MGC de gran éxito. Este SQ-1 es totalmente magnético y cuenta con un contacto súper suave y de plástico a plástico. También se incluyen piezas de tapa de color negro para convertir tu rompecabezas en una variante "sin pegatinas".',
+    categoria:'sq-1',
+    stock:34,
+    cantidadSolicitada:0,
+    precioUnitario:38000,
+    calificacion:4.9,
+    imagen:'./estilos/img/sq1/yjMGCSq1M.webp',
+    disponible:true,
 }]
+let main=document.querySelector('main');
+for(let i=0;i<productos.length;i++){
+    main.insertAdjacentHTML('beforeend',`<section class="product-cards"> <header class="card-header"><img class="card-img" src="${productos[i].imagen}" alt="${productos[i].nombre}"> <h2 class="card-titulo">${productos[i].nombre}</h2></header> <p class="card-descripcion">${productos[i].descripcion}</p> <p class="card-calificacion">${productos[i].calificacion}</p> <p>$${productos[i].precioUnitario}</p> <p>Stock : ${productos[i].stock}</p></section>`);
+}
